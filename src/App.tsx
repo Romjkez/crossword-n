99ad80ed7da4f31aed63c042e78a53d5ae4f17e3
Romjkez <meshkov.ra@ya.ru>
2022-03-07 17:48:31 +0300

    feat(app): setup crossword, initial and success modals

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Crossword from '@jaredreisinger/react-crossword';
+import { CluesInput } from '@jaredreisinger/react-crossword/dist/types';
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
+
+const data: CluesInput = {
+    across: {
+        1: {
+            clue: 'Как зовут кошку Макса?',
+            answer: 'ШПИЛЬКА',
+            row: 3,
+            col: 0,
+        },
+        4: {
+            clue: 'Одноатомный спирт, неотъемлемая часть нашего праздничного стола',
+            answer: 'ЭТАНОЛ',
+            row: 5,
+            col: 4,
+        }
+    },
+    down: {
+        2: {
+            clue: 'Как зовут кошку Ромы?',
+            answer: 'МИЛА',
+            row: 2,
+            col: 2,
+        },
+        3: {
+            clue: 'Имя коллеги, с которой ты не ладишь',
+            answer: 'ОКСАНА',
+            row: 0,
+            col: 6
+        }
+    },
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isIntroModalVisible, setIsIntroModalVisible] = useState(true)
+    const [isSuccessModalVisible, setIsSuccessModalVisible] = useState(false)
+    const [isFinished, setIsFinished] = useState(false)
+    const introModalFooter = <Button label="Погнали!" onClick={() => setIsIntroModalVisible(false)}/>
+    const successModalFooter = <Button label="Ушла требовать" onClick={() => setIsSuccessModalVisible(false)}/>
+
+    function onComplete(isCorrect: boolean) {
+        if (!isFinished && isCorrect) {
+            setIsSuccessModalVisible(true)
+            setIsFinished(true)
+        }
+    }
+
+    return (
+        <div className="app">
+            <Dialog visible={isIntroModalVisible} onHide={() => setIsIntroModalVisible(false)}
+                    header="С Днём Рождения, Настя! 🥳🥳🥳" footer={introModalFooter}>
+                Прежде чем получить подарок, его нужно заслужить! Предлагаем тебе решить кроссворд про тебя и твоих
+                любимых
+                друзей. А там уже посмотрим 🙃
+            </Dialog>
+            <Dialog visible={isSuccessModalVisible} onHide={() => setIsSuccessModalVisible(false)}
+                    header="Урааа! 😊" footer={successModalFooter}>
+                Требуй от своих (надеюсь, что всё еще любимых!) друзей подарок! Только без ногтей и укусов! Но драки
+                подушками и щекотки не запрещены...
+            </Dialog>
+            <Crossword data={data} onCrosswordComplete={onComplete}/>
+        </div>
+    )
 }
 
 export default App;
